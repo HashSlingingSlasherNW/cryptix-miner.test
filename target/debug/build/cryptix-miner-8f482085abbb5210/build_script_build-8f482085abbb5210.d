41f3fdd49a39da813c3b6d299996a86b4526bf2f
Cryptix Network <info@king-krypto.de>
2024-12-23 11:59:31 +0100

    remove target folder

--- target/debug/build/cryptix-miner-8f482085abbb5210/build_script_build-8f482085abbb5210.d
+++
@@ -1,5 +0,0 @@
-F:\Github\cryptix-miner\target\debug\build\cryptix-miner-8f482085abbb5210\build_script_build-8f482085abbb5210.exe: build.rs
-
-F:\Github\cryptix-miner\target\debug\build\cryptix-miner-8f482085abbb5210\build_script_build-8f482085abbb5210.d: build.rs
-
-build.rs:
