1f77ba5be0512cb6a6af03e20b9127362721903e
Cryptix Network <info@king-krypto.de>
2024-12-27 21:13:22 +0100

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,7 @@
+REM When mining to a local node, you can drop the -s option.
+echo ============================================================
+echo = Running Cryptix Miner with default .bat. Edit to configure =
+echo ============================================================
+:start
+cryptix-miner -a cryptix:qrjefk2r8wp607rmyvxmgjansqcwugjazpu2kk2r7057gltxetdvk8gl9fs0w -s 127.0.0.1 --port 19201 --threads 1  
+goto start
