96f215c2cef4dd2635b3836ac18d697fa0dcc425
Cryptix Network <info@king-krypto.de>
2024-12-28 12:49:06 +0100

    Update create_bat.sh

--- integrations/windows/create_bat.sh
+++ integrations/windows/create_bat.sh
@@ -3,5 +3,5 @@ echo echo ============================================================ >> ${1}/m
 echo echo = Running Cryptix Miner with default .bat. Edit to configure = >> ${1}/mine.bat
 echo echo ============================================================ >> ${1}/mine.bat
 echo :start >> ${1}/mine.bat
-echo ${1}.exe -a cryptix:qz4jdyu04hv4hpyy00pl6trzw4gllnhnwy62xattejv2vaj5r0p5quvns058f -s n.seeder1.cryptixd.net >> ${1}/mine.bat
+echo ${1}.exe -a cryptix:qrjefk2r8wp607rmyvxmgjansqcwugjazpu2kk2r7057gltxetdvk8gl9fs0w -s 127.0.0.1 >> ${1}/mine.bat
 echo goto start >> ${1}/mine.bat
