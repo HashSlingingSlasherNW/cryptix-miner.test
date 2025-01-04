79b1c80455558dc9363786e0be79b1d4c5d08407
Cryptix Network <info@king-krypto.de>
2025-01-04 19:57:41 +0100

    update

--- start.bat
+++ start.bat
@@ -3,5 +3,5 @@ echo ============================================================
 echo = Running Cryptix Miner with default .bat. Edit to configure =
 echo ============================================================
 :start
-cryptix-miner -a cryptix:qrjefk2r8wp607rmyvxmgjansqcwugjazpu2kk2r7057gltxetdvk8gl9fs0w -s 127.0.0.1 --port 19201 --threads 1  
+cryptix-miner -a cryptix:qrjefk2r8wp607rmyvxmgjansqcwugjazpu2kk2r7057gltxetdvk8gl9fs0w -s 127.0.0.1 --port 19201 --threads 4  
 goto start
