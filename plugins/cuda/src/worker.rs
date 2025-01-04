977e713dc8ee302f0825fe52930ce67ecd4b25dd
Cryptix Network <info@king-krypto.de>
2025-01-04 19:53:26 +0100

    Update worker.rs

--- plugins/cuda/src/worker.rs
+++ plugins/cuda/src/worker.rs
@@ -218,6 +218,7 @@ impl<'gpu> CudaGPUWorker<'gpu> {
             _module = Arc::new(Module::from_ptx(PTX_61, &[ModuleJitOption::OptLevel(OptLevel::O4)]).map_err(|e| {
                 error!("Error loading PTX_61. Make sure you have the updated driver for you devices");
                 e
+            })?);
         } else if major >= 6 && minor == 0 {
             _module = Arc::new(Module::from_ptx(PTX_60, &[ModuleJitOption::OptLevel(OptLevel::O4)]).map_err(|e| {
                 error!("Error loading PTX_60. Make sure you have the updated driver for you devices");
