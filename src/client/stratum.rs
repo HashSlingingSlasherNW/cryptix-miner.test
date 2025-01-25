c0b3548e409fbbf286399ede59d3ad8815818299
Cryptix Network <info@king-krypto.de>
2025-01-25 13:23:44 +0100

    Update stratum.rs

--- src/client/stratum.rs
+++ src/client/stratum.rs
@@ -313,19 +313,32 @@ impl StratumHandler {
                             self.block_template_ctr
                                 .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |v| Some((v + 1) % 10_000))
                                 .unwrap();
-                            miner
-                                .process_block(Some(PartialBlock {
-                                    id,
-                                    header_hash,
-                                    timestamp,
-                                    nonce: 0,
-                                    target: self.target_pool,
-                                    nonce_mask: self.nonce_mask,
-                                    nonce_fixed: self.nonce_fixed,
-                                    hash: None,
-                                }))
-                                .await
+                            info!("Received block notification: id = {}, header_hash = {:?}, timestamp = {}", id, header_hash, timestamp);
+                        
+                            info!("Process block id: {}, header_hash: {:?}", id, header_hash);
+                            let result = miner.process_block(Some(PartialBlock {
+                                id,
+                                header_hash,
+                                timestamp,
+                                nonce: 0,
+                                target: self.target_pool,
+                                nonce_mask: self.nonce_mask,
+                                nonce_fixed: self.nonce_fixed,
+                                hash: None,
+                            })).await;
+                        
+                            match result {
+                                Ok(_) => {
+                                    info!("block processing completed.");
+                                    Ok(())  
+                                },
+                                Err(e) => {
+                                    error!("Error in block processing: {:?}", e);
+                                    Err(e)  
+                                }
+                            }
                         }
+                                            
                         _ => Err(format!("Unexpected stratum message: {:?}", msg).into()),
                     },
                     _ => Err(format!("Inconsistent stratum message: {:?}", msg).into()),
