d85abac5e35cda2c1b2a3ece2d1ce886f56ca70e
Cryptix Network <info@king-krypto.de>
2025-03-12 19:00:57 +0100

    Update heavy_hash.rs

--- src/pow/heavy_hash.rs
+++ src/pow/heavy_hash.rs
@@ -170,9 +170,8 @@ impl Matrix {
             // ** non-linear memory accesses:**
             for _ in 0..4 { 
 
-                index = (index + (i * 257)) % memory_table.len();  // Base increment
-                index ^= memory_table[index % memory_table.len()] as usize * 19; // XOR with current value
-                index = (index * 73 + i * 41) % memory_table.len(); // Further manipulations
+                index ^= (memory_table[(index * 7 + i) % memory_table.len()] as usize * 19) ^ ((i * 53) % 13);
+                index = (index * 73 + i * 41) % memory_table.len(); 
 
                 // Index paths
                 let shifted = (index.wrapping_add(i * 13)) % memory_table.len();
@@ -180,9 +179,8 @@ impl Matrix {
             }
         }
 
-        // Calculate the final memory-hash result
+        // Final memory-hash result
         for i in 0..32 {
-            // non-linear memory accesses
             let shift_val = (product[i] as usize * 47 + i) % memory_table.len();
             product[i] ^= memory_table[shift_val];
         }
@@ -196,10 +194,13 @@ impl Matrix {
         let mut sbox: [u8; 256] = [0; 256];
 
         // Calculate S-Box with the product value and hash values
-        for i in 0..256 {
-            let mut value = i as u8;
-            value = Self::generate_non_linear_sbox(value, hash_bytes[i % hash_bytes.len()]);
-            sbox[i] = value;
+        for _ in 0..10 {  
+            for i in 0..256 {
+                let mut value = i as u8;
+                value = Self::generate_non_linear_sbox(value, hash_bytes[i % hash_bytes.len()]);
+                value ^= (value << 4) | (value >> 2); 
+                sbox[i] = value;
+            }
         }
 
         // Apply S-Box to the product
