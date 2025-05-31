6eaf487e8c95001bc9e22a18f8aa27ad15091987
Cryptix Network <info@king-krypto.de>
2025-06-01 00:27:40 +0200

    Update build.sh

--- integrations/hiveos/build.sh
+++ integrations/hiveos/build.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+NAME="cryptix_miner_hive_sheet_v029"
 VERSION="v0.2.9"
-BINARY="cryptix-miner"
-TARGET_DIR="cryptix_miner_hive_sheet_v029"
+MINERBIN="cryptix-miner"
 
-./createmanifest.sh "$VERSION" "$BINARY"
-mkdir -p "$TARGET_DIR"
-cp h-manifest.conf *.sh "$BINARY" "$TARGET_DIR/"
-tar cvf "${TARGET_DIR}.tar.gz" "$TARGET_DIR"
+# Create the manifest file
+cat > h-manifest.conf << EOF
+# cryptix-miner manifest for HiveOS
+
+CUSTOM_NAME=$NAME
+CUSTOM_VERSION=$VERSION
+CUSTOM_BUILD=0
+CUSTOM_MINERBIN=$MINERBIN
+CUSTOM_CONFIG_FILENAME=/hive/miners/custom/\$CUSTOM_NAME/config.ini
+CUSTOM_LOG_BASENAME=/var/log/miner/\$CUSTOM_NAME
+WEB_PORT=3338
+EOF
+
+# Prepare the package directory
+mkdir -p $NAME
+
+# Copy all required files into the package directory
+cp h-manifest.conf *.sh $MINERBIN $NAME/
+
+# Create the archive
+tar czf $NAME.tar.gz $NAME
